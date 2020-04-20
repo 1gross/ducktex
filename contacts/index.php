e8daa649a256743fd874c2827471143844d01c2c
Lukmanov Mikhail <lukmanof92@gmail.com>
2020-04-20 19:58:38 +0500

    fix contact

--- contacts/index.php
+++ contacts/index.php
@@ -1,6 +1,7 @@
 <?
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Контакты");
+\Bitrix\Main\Page\Asset::getInstance()->addJs('/local/front/files/js/map.js');
 ?>
     <div class="page contacts">
         <div class="wrapper">
