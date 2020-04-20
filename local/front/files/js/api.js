b0c4c3953a74c65675e944ba17edb2ee3100634f
Lukmanov Mikhail <lukmanof92@gmail.com>
2020-04-20 16:30:56 +0500

    fix event keyup

--- local/front/files/js/api.js
+++ local/front/files/js/api.js
@@ -123,12 +123,13 @@ $(document).ready(function() {
                                      verificationForm.prepend('<input type="hidden" name="SIGN_DATA" value="'+response.sign_data+'">');
                                      phoneDesc.text(response.phone);
                                      
-                                     verificationForm.find('.digit').on('keydown', function () {
+                                     verificationForm.find('.digit').on('keyup', function () {
                                          let verificationCode = '';
                                          verificationForm.find('.digit').each(function () {
                                              verificationCode += $(this).val();
-                                             console.log($(this).val());
+
                                          });
+                                         console.log(verificationCode);
                                          if (verificationCode.length === 6) {
                                              $.ajax({
                                                  url: url,
