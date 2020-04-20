7ca395ef03a1fca936a89d333ec625adf64150cc
Lukmanov Mikhail <lukmanof92@gmail.com>
2020-04-20 20:00:57 +0500

    add jquery include

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -7,6 +7,7 @@ ini_set('error_reporting', E_ALL);
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 */
+CJSCore::Init('jquery');
 require_once __DIR__ . "/constants.php";
 
 require_once __DIR__ . "/classes/B24TechSiteHelper.php";
