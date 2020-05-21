ebe2d6fc448898c8559fb7708b17c194fd3894bd
Lukmanov Mikhail <lukmanof92@gmail.com>
2020-05-21 23:42:30 +0500

    fix

--- local/templates/new/components/bitrix/sale.basket.basket/.default/template.php
+++ local/templates/new/components/bitrix/sale.basket.basket/.default/template.php
@@ -32,9 +32,9 @@ Loc::loadMessages(__FILE__);
                         <div class="last"><?=$ROW['FULL_PRICE_FORMATED']?> / <?=$ROW['MEASURE_TEXT']?></div>
                     <?}?>
                     <?if (floatval($ROW['DISCOUNT_PRICE']) > 0) {?>
-                        <div class="new"><?=$ROW['FULL_PRICE_FORMATED']?> / <?=$ROW['MEASURE_TEXT']?></div>
-                    <?} else {?>
                         <div class="new"><?=$ROW['FULL_PRICE'] - $ROW['DISCOUNT_PRICE']?> руб. / <?=$ROW['MEASURE_TEXT']?></div>
+                    <?} else {?>
+                        <div class="new"><?=$ROW['FULL_PRICE_FORMATED']?> / <?=$ROW['MEASURE_TEXT']?></div>
                     <?}?>
                 </div>
             </td>
