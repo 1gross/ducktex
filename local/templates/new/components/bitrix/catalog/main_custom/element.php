06883c4ae003b64d3080c49e7bee2b21512a5884
Lukmanov Mikhail <lukmanof92@gmail.com>
2020-07-01 10:59:07 +0500

    Merge branch 'master' of https://github.com/luckmann92/ducktex
    # Conflicts:
    #	.htaccess

--- local/templates/new/components/bitrix/catalog/main_custom/element.php
+++ local/templates/new/components/bitrix/catalog/main_custom/element.php
@@ -475,62 +475,65 @@ while ($prod = $rsProduct->GetNextElement()) {
 
 ?>
 <?
-global $arrFilter;
-$GLOBALS['arrFilter']['ID'] = $props;
-$APPLICATION->IncludeComponent(
-    "bitrix:catalog.top",
-    "slider-products-associated",
-    array(
-        "COMPONENT_TEMPLATE" => "slider-products-associated",
-        "IBLOCK_TYPE" => "catalog",
-        "IBLOCK_ID" => "13",
-        "FILTER_NAME" => "arrFilter",
-        "CUSTOM_FILTER" => "",
-        "USE_FILTER" => "Y",
-        "HIDE_NOT_AVAILABLE" => "N",
-        "HIDE_NOT_AVAILABLE_OFFERS" => "N",
-        "ELEMENT_SORT_FIELD" => $_GET["sort"] == "name" ? "name" : "catalog_PRICE_1",
-        "ELEMENT_SORT_ORDER" => $_GET["method"] == "desc" ? "desc" : "asc",
-        "ELEMENT_SORT_FIELD2" => "id",
-        "ELEMENT_SORT_ORDER2" => "desc",
-        "OFFERS_SORT_FIELD" => "sort",
-        "OFFERS_SORT_ORDER" => "asc",
-        "OFFERS_SORT_FIELD2" => "id",
-        "OFFERS_SORT_ORDER2" => "desc",
-        "ELEMENT_COUNT" => "9",
-        "LINE_ELEMENT_COUNT" => "3",
-        "OFFERS_FIELD_CODE" => array(
-            0 => "",
-            1 => "",
+if (!empty($props)) {
+    global $arrFilter;
+    $GLOBALS['arrFilter']['ID'] = $props;
+    $APPLICATION->IncludeComponent(
+        "bitrix:catalog.top",
+        "slider-products-associated",
+        array(
+            "COMPONENT_TEMPLATE" => "slider-products-associated",
+            "IBLOCK_TYPE" => "catalog",
+            "IBLOCK_ID" => "13",
+            "FILTER_NAME" => "arrFilter",
+            "CUSTOM_FILTER" => "",
+            "USE_FILTER" => "Y",
+            "HIDE_NOT_AVAILABLE" => "N",
+            "HIDE_NOT_AVAILABLE_OFFERS" => "N",
+            "ELEMENT_SORT_FIELD" => $_GET["sort"] == "name" ? "name" : "catalog_PRICE_1",
+            "ELEMENT_SORT_ORDER" => $_GET["method"] == "desc" ? "desc" : "asc",
+            "ELEMENT_SORT_FIELD2" => "id",
+            "ELEMENT_SORT_ORDER2" => "desc",
+            "OFFERS_SORT_FIELD" => "sort",
+            "OFFERS_SORT_ORDER" => "asc",
+            "OFFERS_SORT_FIELD2" => "id",
+            "OFFERS_SORT_ORDER2" => "desc",
+            "ELEMENT_COUNT" => "9",
+            "LINE_ELEMENT_COUNT" => "3",
+            "OFFERS_FIELD_CODE" => array(
+                0 => "",
+                1 => "",
+            ),
+            "OFFERS_LIMIT" => "5",
+            "SECTION_URL" => "",
+            "DETAIL_URL" => "",
+            "PRODUCT_QUANTITY_VARIABLE" => "quantity",
+            "SEF_MODE" => "N",
+            "CACHE_TYPE" => "A",
+            "CACHE_TIME" => "36000000",
+            "CACHE_GROUPS" => "Y",
+            "CACHE_FILTER" => "N",
+            "ACTION_VARIABLE" => "action",
+            "PRODUCT_ID_VARIABLE" => "id",
+            "PRICE_CODE" => array(
+                0 => "BASE",
+            ),
+            "USE_PRICE_COUNT" => "N",
+            "SHOW_PRICE_COUNT" => "1",
+            "PRICE_VAT_INCLUDE" => "Y",
+            "CONVERT_CURRENCY" => "N",
+            "BASKET_URL" => "/personal/basket.php",
+            "USE_PRODUCT_QUANTITY" => "N",
+            "ADD_PROPERTIES_TO_BASKET" => "Y",
+            "PRODUCT_PROPS_VARIABLE" => "prop",
+            "PARTIAL_PRODUCT_PROPERTIES" => "N",
+            "DISPLAY_COMPARE" => "N",
+            "COMPATIBLE_MODE" => "Y"
         ),
-        "OFFERS_LIMIT" => "5",
-        "SECTION_URL" => "",
-        "DETAIL_URL" => "",
-        "PRODUCT_QUANTITY_VARIABLE" => "quantity",
-        "SEF_MODE" => "N",
-        "CACHE_TYPE" => "A",
-        "CACHE_TIME" => "36000000",
-        "CACHE_GROUPS" => "Y",
-        "CACHE_FILTER" => "N",
-        "ACTION_VARIABLE" => "action",
-        "PRODUCT_ID_VARIABLE" => "id",
-        "PRICE_CODE" => array(
-            0 => "BASE",
-        ),
-        "USE_PRICE_COUNT" => "N",
-        "SHOW_PRICE_COUNT" => "1",
-        "PRICE_VAT_INCLUDE" => "Y",
-        "CONVERT_CURRENCY" => "N",
-        "BASKET_URL" => "/personal/basket.php",
-        "USE_PRODUCT_QUANTITY" => "N",
-        "ADD_PROPERTIES_TO_BASKET" => "Y",
-        "PRODUCT_PROPS_VARIABLE" => "prop",
-        "PARTIAL_PRODUCT_PROPERTIES" => "N",
-        "DISPLAY_COMPARE" => "N",
-        "COMPATIBLE_MODE" => "Y"
-    ),
-    false
-);
+        false
+    );
+}
+
 $GLOBALS["CATALOG_CURRENT_ELEMENT_ID"] = $ElementID;
 }
 ?>
